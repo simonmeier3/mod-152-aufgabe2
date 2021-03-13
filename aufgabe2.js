f690a00c08dc1ef59e5aa569895316ba1b4ae89c
meie-simon@gmx.ch <68SI25mon>
2021-03-13 11:13:06 +0100

    convert image to different sizes

--- aufgabe2.js
+++ aufgabe2.js
@@ -1,7 +1,11 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 var express = require("express");
 var multer = require("multer");
+var sharp_1 = __importDefault(require("sharp"));
 var app = express();
 var store = multer.diskStorage({
     destination: function (req, file, callback) {
@@ -12,7 +16,48 @@ var store = multer.diskStorage({
     }
 });
 var upload = multer({ storage: store });
-app.post('/api/file', upload.single('file'), function (req, res) {
-    res.json({ success: 'done' });
+app.post('/api/file', upload.single('file'), function (req, res, next) {
+    var filename = req.file.filename;
+    convertingSmall(filename);
+    convertingMedium(filename);
+    convertingLarge(filename);
+    convertingThumbnail(filename);
+    convertingOriginal(filename);
+    res.json("Done");
 });
+function convertingSmall(img) {
+    sharp_1.default(__dirname + '/uploads/' + img)
+        .resize(720, null, {
+        fit: 'contain'
+    })
+        .toFile(__dirname + '/uploads/converted/small_' + img);
+}
+function convertingMedium(img) {
+    sharp_1.default(__dirname + '/uploads/' + img)
+        .resize(1280, null, {
+        fit: 'contain'
+    })
+        .toFile(__dirname + '/uploads/converted/medium_' + img);
+}
+function convertingLarge(img) {
+    sharp_1.default(__dirname + '/uploads/' + img)
+        .resize(1920, null, {
+        fit: 'contain'
+    })
+        .toFile(__dirname + '/uploads/converted/large_' + img);
+}
+function convertingThumbnail(img) {
+    sharp_1.default(__dirname + '/uploads/' + img)
+        .resize(360, null, {
+        fit: 'contain'
+    })
+        .toFile(__dirname + '/uploads/converted/thumbnail_' + img);
+}
+function convertingOriginal(img) {
+    sharp_1.default(__dirname + '/uploads/' + img)
+        .resize(null, null, {
+        fit: 'contain'
+    })
+        .toFile(__dirname + '/uploads/converted/original_' + img);
+}
 app.listen(3000);
--- aufgabe2.ts
+++ aufgabe2.ts
@@ -1,5 +1,6 @@
 import express = require('express');
 import multer = require("multer");
+import sharp from "sharp";
 
 let app = express();
 
@@ -14,9 +15,55 @@ let store = multer.diskStorage({
 
 let upload = multer({storage: store});
 
-app.post('/api/file', upload.single('file'),(req, res) => {
-    res.json({success: 'done'});
-})
+app.post('/api/file', upload.single('file'), (req,res,next ) => {
+
+    let filename = req.file.filename;
+    convertingSmall(filename);
+    convertingMedium(filename);
+    convertingLarge(filename);
+    convertingThumbnail(filename);
+    convertingOriginal(filename);
+    res.json("Done");
+});
+
+function convertingSmall(img: string){
+sharp(__dirname + '/uploads/' + img)
+    .resize(720, null, {
+        fit: 'contain'
+    })
+    .toFile(__dirname + '/uploads/converted/small_' + img);
+}
+function convertingMedium(img: string){
+    sharp(__dirname + '/uploads/' + img)
+        .resize(1280, null, {
+            fit: 'contain'
+        })
+        .toFile(__dirname + '/uploads/converted/medium_' + img);
+}
+function convertingLarge(img: string){
+    sharp(__dirname + '/uploads/' + img)
+        .resize(1920, null, {
+            fit: 'contain'
+        })
+        .toFile(__dirname + '/uploads/converted/large_' + img);
+}
+function convertingThumbnail(img: string){
+    sharp(__dirname + '/uploads/' + img)
+        .resize(360, null, {
+            fit: 'contain'
+        })
+        .toFile(__dirname + '/uploads/converted/thumbnail_' + img);
+}
+
+function convertingOriginal(img: string){
+    sharp(__dirname + '/uploads/' + img)
+        .resize(null, null, {
+            fit: 'contain'
+        })
+        .toFile(__dirname + '/uploads/converted/original_' + img);
+}
+
+
 
 app.listen(3000);
 
